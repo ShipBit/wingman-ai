14f151189180886921348c1238294cb27dc50565
Simon Hopstätter <simon.hopstaetter@shipbit.de>
2023-11-17 00:53:05 +0100

    fix casing

--- wingmen/edge_tts_groot.py
+++ wingmen/edge_tts_groot.py
@@ -2,7 +2,7 @@ from wingmen.wingman import Wingman
 from services.edge import EdgeTTS
 
 
-class EdgeTTSGroot(Wingman):
+class EdgeTtsGroot(Wingman):
     async def process(self, _audio_input_wav: str):
         edge_tts = EdgeTTS()
         await edge_tts.generate_speech("I am Groot", filename="groot.mp3")
