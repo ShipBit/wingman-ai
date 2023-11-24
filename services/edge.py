7850657864fa6519d344789caf1150c41a1eee63
Simon Hopstätter <simon.hopstaetter@shipbit.de>
2023-11-24 22:21:54 +0100

    implement EdgeTTS language detection and switch

--- services/edge.py
+++ services/edge.py
@@ -1,5 +1,6 @@
-from edge_tts import Communicate, VoicesManager
 import random
+from edge_tts import Communicate, VoicesManager
+from services.printr import Printr
 
 # List available voices in your terminal using 'edge-tts --list-voices'.
 #
@@ -9,6 +10,9 @@ import random
 
 
 class EdgeTTS:
+    def __init__(self):
+        self.random_voices = {}
+
     async def generate_speech(
         self,
         text: str,
@@ -24,8 +28,28 @@ class EdgeTTS:
 
         return communicate
 
-    async def get_random_voice(self, gender: str = "Male", language: str = "en") -> str:
+    async def get_random_voice(
+        self, gender: str = "Male", locale: str = "en-US"
+    ) -> str:
         voices = await VoicesManager.create()
-        voice = voices.find(Gender=gender, Language=language)
+        voice = voices.find(Gender=gender, Locale=locale)
         random_voice = random.choice(voice)
-        return random_voice.get("Name")
+        return random_voice.get("ShortName")
+
+    async def get_same_random_voice_for_language(
+        self, gender="Male", locale: str = "en-US"
+    ) -> str:
+        # if we already have a voice for this language, return it ("cache")
+        # Otherwise the voice would change every time we call this function / talk to the AI
+        if self.random_voices.get(locale):
+            return self.random_voices[locale]
+
+        random_voice = await self.get_random_voice(
+            locale=locale,
+            gender=gender,
+        )
+        self.random_voices[locale] = random_voice
+
+        Printr.hl_print(f"   Your random EdgeTTS voice: '{random_voice}'.", False)
+
+        return random_voice
