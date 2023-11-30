c79465f11a0e0da94a6cb931b2e1a20caa934a93
Tobias Dobbrunz <tobias.dobbrunz@shipbit.de>
2023-11-30 12:46:56 +0100

    add version to header
    Signed-off-by: Tobias Dobbrunz <tobias.dobbrunz@shipbit.de>

--- gui/header.py
+++ gui/header.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from gui.components.icon import Icon
 from gui.components.icon_button import IconButton
+from services.check_version import LOCAL_VERSION
 
 class Header(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
@@ -12,10 +13,15 @@ class Header(ctk.CTkFrame):
         self.logo = ctk.CTkLabel(self, text="", image=txt_logo)
         self.logo.grid(row=0, column=0, padx=5, pady=5, sticky="w")
 
-        #TODO just for debugging -> move to config
+        self.version = ctk.CTkLabel(self, text=LOCAL_VERSION)
+        self.version.grid(row=0, column=1, padx=5, pady=5, sticky="e")
+
+        # TODO: add links for 'discord', 'patreon' and 'github'
+
+        # TODO: just for debugging -> move to config
         self.appearance_switcher = ctk.CTkSegmentedButton(self, values=["🌙", "⚙️", "☀️"], command=self.set_appearance)
         self.appearance_switcher.set("⚙️")
-        self.appearance_switcher.grid(row=0, column=1, padx=5, pady=5, sticky="e")
+        self.appearance_switcher.grid(row=0, column=2, padx=5, pady=5, sticky="e")
 
         self.button = IconButton(self,
                                 icon="✖️",
@@ -23,10 +29,10 @@ class Header(ctk.CTkFrame):
                                 size=32,
                                 themed=False,
                                 command=master.quit)
-        self.button.grid(row=0, column=2, padx=5, pady=5, sticky="e")
+        self.button.grid(row=0, column=3, padx=5, pady=5, sticky="e")
 
     def set_appearance(self, value):
-        #TODO just for debugging
+        # TODO: just for debugging
         # also: should be an enum
 
         match value:
