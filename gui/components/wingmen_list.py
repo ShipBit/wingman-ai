24cd1a7f163246459917bc90062b835c2023ab7b
Timo Korinth <info@timokorinth.de>
2023-12-04 12:26:01 +0100

    update some info texts

--- gui/components/wingmen_list.py
+++ gui/components/wingmen_list.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 
+
 class WingmenList(ctk.CTkFrame):
     def __init__(self, master, wingmen, broken=False, **kwargs):
         super().__init__(master, **kwargs)
@@ -18,17 +19,27 @@ class WingmenList(ctk.CTkFrame):
             self.intro_label = ctk.CTkLabel(self, text=intro)
             self.intro_label.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-            for i, wingman in enumerate(wingmen):
-                row_index = i + 1
+            self.header_label = ctk.CTkLabel(
+                self, text="Activation Key", text_color=("gray40", "gray55")
+            )
+            self.header_label.grid(row=1, column=0, padx=10)
 
-                key = wingman['name'] if broken else wingman.get_record_key()
+            self.header_label2 = ctk.CTkLabel(
+                self, text="Wingman to activate", text_color=("gray40", "gray55")
+            )
+            self.header_label2.grid(row=1, column=2, padx=10, sticky="w")
+
+            for i, wingman in enumerate(wingmen):
+                row_index = i + 2
+
+                key = wingman["name"] if broken else wingman.get_record_key()
                 key_label = ctk.CTkLabel(self, text=key)
                 key_label.grid(row=row_index, column=0, padx=10)
 
                 delimiter_label = ctk.CTkLabel(self, text="〉")
                 delimiter_label.grid(row=row_index, column=1, padx=10)
 
-                value = wingman['error'] if broken else wingman.name
+                value = wingman["error"] if broken else wingman.name
                 value_label = ctk.CTkLabel(self, text=value)
                 value_label.grid(row=row_index, column=2, padx=10, sticky="w")
                 self.wingmen_list.append([key_label, delimiter_label, value_label])
@@ -36,5 +47,8 @@ class WingmenList(ctk.CTkFrame):
             if not broken:
                 self.warning_label = ctk.CTkLabel(self, text="⚠️ WARNING ⚠️")
                 self.warning_label.grid(row=0, column=1, padx=10, pady=10)
-                self.warning_msg = ctk.CTkLabel(self, text="Seems like you don't have any functional wingmen registered.")
+                self.warning_msg = ctk.CTkLabel(
+                    self,
+                    text="Seems like you don't have any functional wingmen registered.",
+                )
                 self.warning_msg.grid(row=1, column=1, padx=10, pady=0)
