34d32eac370aec368b380ce7a11d61f18304b2b0
Simon Hopstätter <simon.hopstaetter@shipbit.de>
2023-12-08 10:25:07 +0100

    add new effects by @JaydiCodes

--- services/sound_effects.py
+++ services/sound_effects.py
@@ -3,6 +3,7 @@ from pedalboard import (
     Compressor,
     HighpassFilter,
     LowpassFilter,
+    PeakFilter,
     Pedalboard,
     Chorus,
     PitchShift,
@@ -13,7 +14,7 @@ from pedalboard import (
 )
 
 
-# Credits to Discord community member @psigen!
+# Credits to Discord community member @psigen aka GH @JaydiCodes!
 class SoundEffects(Enum):
     ROBOT = Pedalboard(
         [
@@ -36,10 +37,40 @@ class SoundEffects(Enum):
             Gain(gain_db=6),
         ]
     )
+    INTERIOR_HELMET = Pedalboard(
+        [
+            PeakFilter(1000, 6, 2),
+            Delay(delay_seconds=0.01, mix=0.02),
+            Reverb(
+                room_size=0.01,
+                damping=0.9,
+                dry_level=0.8,
+                wet_level=0.2,
+                freeze_mode=1,
+                width=0.05,
+            ),
+        ]
+    )
     INTERIOR_SMALL = Pedalboard(
         [
-            Delay(delay_seconds=0.1, mix=0.9),
-            Reverb(room_size=0.05, dry_level=0.5, wet_level=0.3, width=0.2),
+            Delay(delay_seconds=0.03, mix=0.05),
+            Reverb(
+                room_size=0.03, damping=0.7, dry_level=0.7, wet_level=0.3, width=0.1
+            ),
+        ]
+    )
+    INTERIOR_MEDIUM = Pedalboard(
+        [
+            Delay(delay_seconds=0.09, mix=0.07),
+            Reverb(
+                room_size=0.05, damping=0.6, dry_level=0.6, wet_level=0.4, width=0.2
+            ),
+        ]
+    )
+    INTERIOR_LARGE = Pedalboard(
+        [
+            Delay(delay_seconds=0.2, mix=0.1),
+            Reverb(room_size=0.2, dry_level=0.5, wet_level=0.5, width=0.5),
         ]
     )
 
@@ -54,7 +85,10 @@ def get_sound_effects_from_config(config: dict):
     mapping = {
         "ROBOT": SoundEffects.ROBOT.value,
         "RADIO": SoundEffects.RADIO.value,
+        "INTERIOR_HELMET": SoundEffects.INTERIOR_HELMET.value,
         "INTERIOR_SMALL": SoundEffects.INTERIOR_SMALL.value,
+        "INTERIOR_MEDIUM": SoundEffects.INTERIOR_MEDIUM.value,
+        "INTERIOR_LARGE": SoundEffects.INTERIOR_LARGE.value,
     }
 
     for effect_name in sound_effects_config:
