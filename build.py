6d1b91e25d9881cfc7ac312d9cd9d2196b4104c8
Simon Hopstätter <simon.hopstaetter@shipbit.de>
2023-12-14 13:42:03 +0100

    rename backend to WingmanAICore.exe

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ cmd = [
     "PyInstaller",
     "main.py",  # your main file with ui.run()
     "--name",
-    "WingmanAI",  # name of your app
+    "WingmanAiCore",  # name of your app
     # "--onefile",
     # "--windowed",  # prevent console appearing, only use with ui.run(native=True, ...)
     "--icon",
