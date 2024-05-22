b2a41b7becfa9639e10a994b2dd969b10e9ef495
Simon Hopstätter <simon.hopstaetter@shipbit.de>
2024-05-22 12:31:44 +0200

    bump version to 1.3.1

--- services/system_manager.py
+++ services/system_manager.py
@@ -4,7 +4,7 @@ import requests
 from packaging import version
 from api.interface import SystemCore, SystemInfo
 
-LOCAL_VERSION = "1.3.0"
+LOCAL_VERSION = "1.3.1"
 VERSION_ENDPOINT = "https://shipbit.de/wingman.json"
 
 
