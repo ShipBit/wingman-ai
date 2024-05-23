bb3dd0e4f8d59b021b6c5f67bec3d22d0a3b4ab3
Simon Hopstätter <simon.hopstaetter@shipbit.de>
2024-05-23 23:31:16 +0200

    update sfx credits & auto-format latest changes

--- services/sound_effects.py
+++ services/sound_effects.py
@@ -2,7 +2,6 @@ from enum import Enum
 from pedalboard import (
     HighpassFilter,
     LowpassFilter,
-    PeakFilter,
     Pedalboard,
     Chorus,
     Resample,
@@ -11,62 +10,105 @@ from pedalboard import (
     Gain,
     Bitcrush,
     Compressor,
-    Distortion,
 )
 from api.interface import SoundConfig
 
 
-# Credits to Discord community member @psigen aka GH @JaydiCodes!
+# Credits to our community members @JaydiCodes and @Thaendril!
 class SoundEffects(Enum):
     ROBOT = Pedalboard(
         [
-        Bitcrush(bit_depth=12),     # Moderate bitcrusher effect for subtle digital tone
-        Chorus(rate_hz=1.5, depth=0.6, mix=0.4, centre_delay_ms=10, feedback=0.2),  # Smooth chorus for subtle modulation
-        Reverb(room_size=0.1, dry_level=0.8, wet_level=0.2, freeze_mode=0.0, width=0.3),  # Light reverb for slight spatial enhancement
-        Delay(delay_seconds=0.01, feedback=0.1, mix=0.1),  # Very subtle delay for slight echo
-        Gain(gain_db=-1)  # Careful with gain, it adds presence but can cause peaking. 
-    
+            Bitcrush(
+                bit_depth=12
+            ),  # Moderate bitcrusher effect for subtle digital tone
+            Chorus(
+                rate_hz=1.5, depth=0.6, mix=0.4, centre_delay_ms=10, feedback=0.2
+            ),  # Smooth chorus for subtle modulation
+            Reverb(
+                room_size=0.1, dry_level=0.8, wet_level=0.2, freeze_mode=0.0, width=0.3
+            ),  # Light reverb for slight spatial enhancement
+            Delay(
+                delay_seconds=0.01, feedback=0.1, mix=0.1
+            ),  # Very subtle delay for slight echo
+            Gain(
+                gain_db=-1
+            ),  # Careful with gain, it adds presence but can cause peaking.
         ]
     )
-    
+
     RADIO = Pedalboard(
         [
-        HighpassFilter(cutoff_frequency_hz=800),  # Slightly lower high-pass filter to cut off low frequencies
-        LowpassFilter(cutoff_frequency_hz=4000),  # Slightly lower low-pass filter to cut off high frequencies
-        Resample(target_sample_rate=8000),  # Lower resample rate to simulate radio bandwidth more closely
-        Compressor(threshold_db=-18, ratio=6),  # Adjust compressor settings for a tighter dynamic range
-        Gain(gain_db=75)  # Increase gain to ensure presence
+            HighpassFilter(
+                cutoff_frequency_hz=800
+            ),  # Slightly lower high-pass filter to cut off low frequencies
+            LowpassFilter(
+                cutoff_frequency_hz=4000
+            ),  # Slightly lower low-pass filter to cut off high frequencies
+            Resample(
+                target_sample_rate=8000
+            ),  # Lower resample rate to simulate radio bandwidth more closely
+            Compressor(
+                threshold_db=-18, ratio=6
+            ),  # Adjust compressor settings for a tighter dynamic range
+            Gain(gain_db=75),  # Increase gain to ensure presence
         ]
     )
     INTERIOR_HELMET = Pedalboard(
         [
-        HighpassFilter(cutoff_frequency_hz=400),  # High-pass filter to cut off low frequencies
-        LowpassFilter(cutoff_frequency_hz=8000),  # Low-pass filter to retain high frequencies
-        Compressor(threshold_db=-20, ratio=4),  # Compressor to add dynamic range compression
-        Delay(delay_seconds=0.01, feedback=0.1, mix=0.2),  # Subtle delay to simulate the enclosed space
-        Reverb(room_size=0.1, damping=0.7, wet_level=0.2, dry_level=0.8, width=0.3, freeze_mode=0.0),  # Subtle reverb to enhance helmet effect
-        Gain(gain_db=65)  # Moderate gain to ensure presence
+            HighpassFilter(
+                cutoff_frequency_hz=400
+            ),  # High-pass filter to cut off low frequencies
+            LowpassFilter(
+                cutoff_frequency_hz=8000
+            ),  # Low-pass filter to retain high frequencies
+            Compressor(
+                threshold_db=-20, ratio=4
+            ),  # Compressor to add dynamic range compression
+            Delay(
+                delay_seconds=0.01, feedback=0.1, mix=0.2
+            ),  # Subtle delay to simulate the enclosed space
+            Reverb(
+                room_size=0.1,
+                damping=0.7,
+                wet_level=0.2,
+                dry_level=0.8,
+                width=0.3,
+                freeze_mode=0.0,
+            ),  # Subtle reverb to enhance helmet effect
+            Gain(gain_db=65),  # Moderate gain to ensure presence
         ]
     )
     INTERIOR_SMALL = Pedalboard(
         [
-        Delay(delay_seconds=0.03, mix=0.05),  # Subtle delay to simulate room reflections
-        Reverb(room_size=0.03, damping=0.7, dry_level=0.7, wet_level=0.3, width=0.1),  # Reverb to enhance room effect
-        Gain(gain_db=-3) #Reduced to solve clipping
+            Delay(
+                delay_seconds=0.03, mix=0.05
+            ),  # Subtle delay to simulate room reflections
+            Reverb(
+                room_size=0.03, damping=0.7, dry_level=0.7, wet_level=0.3, width=0.1
+            ),  # Reverb to enhance room effect
+            Gain(gain_db=-3),  # Reduced to solve clipping
         ]
     )
     INTERIOR_MEDIUM = Pedalboard(
         [
-        Delay(delay_seconds=0.05, mix=0.05),  # Subtle delay to simulate room reflections
-        Reverb(room_size=0.5, damping=0.5, dry_level=0.7, wet_level=0.3, width=0.5),  # Reverb to enhance room effect
-        Gain(gain_db=-3)  # Slight reduction in gain to prevent clipping
+            Delay(
+                delay_seconds=0.05, mix=0.05
+            ),  # Subtle delay to simulate room reflections
+            Reverb(
+                room_size=0.5, damping=0.5, dry_level=0.7, wet_level=0.3, width=0.5
+            ),  # Reverb to enhance room effect
+            Gain(gain_db=-3),  # Slight reduction in gain to prevent clipping
         ]
     )
     INTERIOR_LARGE = Pedalboard(
         [
-        Delay(delay_seconds=0.07, mix=0.1),  # Subtle delay to simulate large room reflections
-        Reverb(room_size=0.7, damping=0.5, dry_level=0.7, wet_level=0.3, width=0.8),  # Reverb to enhance large room effect
-        Gain(gain_db=-3)  # Slight reduction in gain to prevent clipping
+            Delay(
+                delay_seconds=0.07, mix=0.1
+            ),  # Subtle delay to simulate large room reflections
+            Reverb(
+                room_size=0.7, damping=0.5, dry_level=0.7, wet_level=0.3, width=0.8
+            ),  # Reverb to enhance large room effect
+            Gain(gain_db=-3),  # Slight reduction in gain to prevent clipping
         ]
     )
 
