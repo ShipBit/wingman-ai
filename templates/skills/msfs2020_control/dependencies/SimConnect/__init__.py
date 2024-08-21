8e7dee1cd4e049b2f0fc227cc05b95a3e606d29e
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2024-08-21 10:05:55 -0400

    Added MSFS 2020 skill (#238)
    * add flight simulator skill for MSFS2020
    
    -connect with SimConnect
    
    -add loop option for "tour guide" mode
    
    -needs new category "flight_ simulator" added
    
    * Add flight simulator category for skill

---
+++ templates/skills/msfs2020_control/dependencies/SimConnect/__init__.py
@@ -0,0 +1,17 @@
+from .SimConnect import SimConnect, millis, DWORD
+from .RequestList import AircraftRequests, Request
+from .EventList import AircraftEvents, Event
+from .FacilitiesList import FacilitiesRequests, Facilitie
+
+
+def int_or_str(value):
+	try:
+		return int(value)
+	except TypeError:
+		return value
+
+
+__version__ = "0.4.26"
+VERSION = tuple(map(int_or_str, __version__.split(".")))
+
+__all__ = ["SimConnect", "Request", "Event", "millis", "DWORD", "AircraftRequests", "AircraftEvents", "FacilitiesRequests"]
