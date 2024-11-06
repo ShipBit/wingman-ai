55f293d0582471bdcc03dac7622443a5105f2dd2
Timo Korinth <info@timokorinth.de>
2024-11-06 20:30:25 +0100

    add first minecraft bot prototype

---
+++ skills/minecraft_bot/main.py
@@ -0,0 +1,93 @@
+from typing import TYPE_CHECKING
+from api.enums import LogType
+from api.interface import (
+    SettingsConfig,
+    SkillConfig,
+    WingmanInitializationError,
+)
+from skills.skill_base import Skill
+
+from javascript import require, On
+mineflayer = require('mineflayer')
+pathfinder = require('mineflayer-pathfinder')
+
+if TYPE_CHECKING:
+    from wingmen.open_ai_wingman import OpenAiWingman
+
+class MinecraftBot(Skill):
+    def __init__(
+        self,
+        config: SkillConfig,
+        settings: SettingsConfig,
+        wingman: "OpenAiWingman",
+    ) -> None:
+        super().__init__(config=config, settings=settings, wingman=wingman)
+
+        self.bot = None
+
+    async def validate(self) -> list[WingmanInitializationError]:
+        errors = await super().validate()
+        return errors
+
+    def get_tools(self) -> list[tuple[str, dict]]:
+        tools = [
+            (
+                "join_spawn_game",
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "join_spawn_game",
+                        "description": "Join a minecraft game and spawn as character.",
+                    },
+                },
+            ),
+            (
+                "go_to_player",
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "go_to_player",
+                        "description": "Go to the player.",
+                    },
+                },
+            ),
+        ]
+        return tools
+    
+    async def init_mineflayer(self):
+        self.bot = mineflayer.createBot({
+            'host': '127.0.0.1',
+            'port': 57440,
+            'username': 'Steve'
+        })
+        self.bot.loadPlugin(pathfinder.pathfinder)
+
+    async def go_to_player(self):
+        movements = pathfinder.Movements(self.bot)
+        player = self.bot.players['Yoda3001']
+        print(self.bot.players)
+        target = player.entity
+        if not target:
+            self.bot.chat("I don't see you !")
+            return
+
+        pos = target.position
+        self.bot.pathfinder.setMovements(movements)
+        self.bot.pathfinder.setGoal(pathfinder.goals.GoalNear(pos.x, pos.y, pos.z, 1))
+
+    async def execute_tool(self, tool_name: str, parameters: dict[str, any]) -> tuple[str, str]:
+        function_response = ""
+        instant_response = ""
+
+        if tool_name == "join_spawn_game":
+            await self.init_mineflayer()
+            function_response = "Joined your game."
+
+        if tool_name == "go_to_player":
+            await self.go_to_player()
+            function_response = "On my way."
+
+        if self.settings.debug_mode:
+            await self.printr.print_async(f"Executed {tool_name} with parameters {parameters}. Result: {function_response}", color=LogType.INFO)
+
+        return function_response, instant_response
