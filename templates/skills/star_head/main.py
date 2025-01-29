5f0ced55056f160aa7aa5819ea95a9c2d6580f60
Timo Korinth <info@timokorinth.de>
2025-01-29 10:23:31 +0100

    update template

--- templates/skills/star_head/main.py
+++ templates/skills/star_head/main.py
@@ -37,6 +37,9 @@ class StarHead(Skill):
         self.celestial_objects = []
         self.celestial_object_names = []
         self.quantum_drives = []
+        self.shops = []
+        self.shop_names = []
+        self.shop_parent_names = []
 
     async def validate(self) -> list[WingmanInitializationError]:
         errors = await super().validate()
@@ -78,6 +81,19 @@ class StarHead(Skill):
             "vehiclecomponent", {"typeFilter": 8}
         )
 
+        self.shops = await self._fetch_data("shop")
+        self.shop_names = [shop["name"] for shop in self.shops]
+
+        # Remove duplicate shop names
+        self.shop_names = list(dict.fromkeys(self.shop_names))
+
+        self.shop_parent_names = [
+            shop["parent"]["name"] for shop in self.shops if shop["parent"]
+        ]
+
+        # Remove duplicate parent names
+        self.shop_parent_names = list(dict.fromkeys(self.shop_parent_names))
+
     async def _fetch_data(
         self, endpoint: str, params: Optional[dict[str, any]] = None
     ) -> list[dict[str, any]]:
@@ -113,6 +129,17 @@ class StarHead(Skill):
             function_response = await self._get_best_trading_route(**parameters)
         if tool_name == "get_ship_information":
             function_response = await self._get_ship_information(**parameters)
+        if tool_name == "get_trading_information_of_specific_shop":
+            function_response = await self._get_trading_information_of_specific_shop(
+                **parameters
+            )
+        if tool_name == "get_trading_shop_information_for_celestial_objects":
+            function_response = (
+                await self._get_trading_shop_information_for_celestial_objects(
+                    **parameters
+                )
+            )
+
         return function_response, instant_response
 
     async def is_waiting_response_needed(self, tool_name: str) -> bool:
@@ -135,9 +162,9 @@ class StarHead(Skill):
                                     "type": "string",
                                     "enum": self.celestial_object_names,
                                 },
-                                "moneyToSpend": {"type": "number"},
+                                "money_to_spend": {"type": "number"},
                             },
-                            "required": ["ship", "position", "moneyToSpend"],
+                            "required": ["ship", "position", "money_to_spend"],
                         },
                     },
                 },
@@ -159,9 +186,93 @@ class StarHead(Skill):
                     },
                 },
             ),
+            (
+                "get_trading_information_of_specific_shop",
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "get_trading_information_of_specific_shop",
+                        "description": "Gives trading information about the given shop, like which commodities you can sell or buy and for which price. If the name of the shop is not unique, you have to specify the celestial object.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "shop": {"type": "string", "enum": self.shop_names},
+                            },
+                            "required": ["shop"],
+                        },
+                    },
+                },
+            ),
+            (
+                "get_trading_shop_information_for_celestial_objects",
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "get_trading_shop_information_for_celestial_objects",
+                        "description": "Gives trading information about the given celestial object, like which commodities you can sell or buy at which shop and for which price. All shops with shop items on that celestial object will be returned.",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "celestial_object": {
+                                    "type": "string",
+                                    "enum": self.celestial_object_names,
+                                },
+                            },
+                            "required": ["celestial_object"],
+                        },
+                    },
+                },
+            ),
         ]
 
         return tools
+
+    async def _get_trading_shop_information_for_celestial_objects(
+        self, celestial_object: str
+    ) -> str:
+        object_id = self._get_celestial_object_id(celestial_object)
+
+        if not object_id:
+            return f"Could not find celestial object '{celestial_object}' in the StarHead database."
+
+        shops = await self._fetch_data(f"shop?celestialObjectFilter={object_id}")
+
+        shop_items = {}
+        for shop in shops:
+            items = await self._fetch_data(f"shop/{shop['id']}/items")
+            for item in items:
+                item["pricePerItem"] = item["pricePerItem"] * 100
+                item["tradeType"] = (
+                    "Sold by store" if item["tradeType"] == "buy" else "The shop buys"
+                )
+            shop_items[f"{shop['parent']['name']} - {shop['name']}"] = items
+
+        shop_details = json.dumps(shop_items)
+        return shop_details
+
+    async def _get_trading_information_of_specific_shop(self, shop: str = None) -> str:
+        # Get all shops with the given name
+        shops = [
+            shop for shop in self.shops if shop["name"].lower() == shop["name"].lower()
+        ]
+
+        # Check if there are multiple shops with the same name
+        if len(shops) > 1:
+            return f"Multiple shops with the name '{shop}' found. Please specify the celestial object."
+
+        if not shops:
+            return f"Could not find shop '{shop}' in the StarHead database."
+
+        shop_items = {}
+
+        for shop in shops:
+            items = await self._fetch_data(f"shop/{shop['id']}/items")
+            for item in items:
+                item["pricePerItem"] = item["pricePerItem"] * 100
+            shop_items[shop["name"]] = items
+
+        shop_details = json.dumps(items)
+        return shop_details
 
     async def _get_ship_information(self, ship: str) -> str:
         try:
@@ -177,7 +288,7 @@ class StarHead(Skill):
         return ship_details
 
     async def _get_best_trading_route(
-        self, ship: str, position: str, moneyToSpend: float
+        self, ship: str, position: str, money_to_spend: float
     ) -> str:
         """Calculates the best trading route for the specified ship and position.
         Note that the function arguments have to match the funtion_args from OpenAI, hence the camelCase!
@@ -195,7 +306,7 @@ class StarHead(Skill):
             "startCelestialObjectId": celestial_object_id,
             "quantumDriveId": qd["id"] if qd else None,
             "maxAvailablScu": cargo,
-            "maxAvailableMoney": moneyToSpend,
+            "maxAvailableMoney": money_to_spend,
             "useOnlyWeaponFreeZones": False,
             "onlySingleSections": True,
         }
@@ -215,7 +326,7 @@ class StarHead(Skill):
         if parsed_response:
             section = parsed_response[0]
             return json.dumps(section)
-        return f"No route found for ship '{ship}' at '{position}' with '{moneyToSpend}' aUEC."
+        return f"No route found for ship '{ship}' at '{position}' with '{money_to_spend}' aUEC."
 
     def _get_celestial_object_id(self, name: str) -> Optional[int]:
         """Finds the ID of the celestial object with the specified name."""
