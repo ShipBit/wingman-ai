18518fea9583e07a850a97c074d76683cd5176cd
Timo Korinth <info@timokorinth.de>
2025-01-29 10:24:55 +0100

    update StarHead skill (#278)
    * add shop info
    
    * update prompt
    
    * try another approach
    
    * update function naming
    
    * fix naming
    
    * first try to integrate vision skill in starhead
    
    * update prompts and image generation skill
    
    * refactor after rebase
    
    * remove vision functions for now
    
    * update template

--- skills/image_generation/main.py
+++ skills/image_generation/main.py
@@ -39,36 +39,40 @@ class ImageGeneration(Skill):
 
         if tool_name == "generate_image":
             prompt = parameters["prompt"]
-            if self.settings.debug_mode:
-                await self.printr.print_async(f"Generate image with prompt: {prompt}.", color=LogType.INFO)
-            image = await self.wingman.generate_image(prompt)
-            await self.printr.print_async(
-                        "",
-                        color=LogType.INFO,
-                        source=LogSource.WINGMAN,
-                        source_name=self.wingman.name,
-                        skill_name=self.name,
-                        additional_data={"image_url": image},
-                    )
-            if image:
-                function_response = "Here is an image based on your prompt."
-
-                if self.save_images:
-                    image_path = path.join(
-                        self.image_path,
-                        f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{prompt[:40]}.png"
-                    )
-                    image_response = requests.get(image)
-
-                    if image_response.status_code == 200:
-                        with open(image_path, 'wb') as file:
-                            file.write(image_response.content)
-
-                        function_response += f" The image has also been stored to {image_path}."
-                        if self.settings.debug_mode:
-                            await self.printr.print_async(f"Image displayed and saved at {image_path}.", color=LogType.INFO)
-
+            await self.generate_image(prompt)
+            function_response = "Here is an image based on your prompt."
         return function_response, instant_response
+    
+    async def generate_image(self, prompt: str) -> str:
+        if self.settings.debug_mode:
+            await self.printr.print_async(f"Generate image with prompt: {prompt}.", color=LogType.INFO)
+
+        image = await self.wingman.generate_image(prompt)
+        await self.printr.print_async(
+                    "",
+                    color=LogType.INFO,
+                    source=LogSource.WINGMAN,
+                    source_name=self.wingman.name,
+                    skill_name=self.name,
+                    additional_data={"image_url": image},
+                )
+        if image:
+            function_response = "Here is an image based on your prompt."
+
+            if self.save_images:
+                image_path = path.join(
+                    self.image_path,
+                    f"{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}_{prompt[:40]}.png"
+                )
+                image_response = requests.get(image)
+
+                if image_response.status_code == 200:
+                    with open(image_path, 'wb') as file:
+                        file.write(image_response.content)
+
+                    function_response += f" The image has also been stored to {image_path}."
+                    if self.settings.debug_mode:
+                        await self.printr.print_async(f"Image displayed and saved at {image_path}.", color=LogType.INFO)
 
     async def is_waiting_response_needed(self, tool_name: str) -> bool:
         return True
