f4a94f489d8cdd68a7ef5f1e3a6172122626466f
Simon Hopstätter <simon.hopstaetter@shipbit.de>
2025-06-04 11:18:14 +0200

    fix printr use, minor issues and formatting

--- providers/xtts2.py
+++ providers/xtts2.py
@@ -3,14 +3,19 @@ from pathlib import Path
 import json
 import gc
 import requests
-from api.interface import XTTS2Settings, XTTS2TtsConfig, SoundConfig # Needs to be created
-from services.audio_player import AudioPlayer
-from services.file import get_writable_dir
-from services.printr import Printr
 import torch
 import torchaudio
 import numpy as np
 from TTS.api import TTS
+from api.interface import (
+    XTTS2Settings,
+    XTTS2TtsConfig,
+    SoundConfig,
+)  # Needs to be created
+from services.audio_player import AudioPlayer
+from services.file import get_writable_dir
+from services.printr import Printr
+
 
 # To do - maybe allow custom latents and cloning paths for super advanced users
 # To do - maybe allow direct use of mantella or other XTTS2 servers for super advanced users, input IP address like external provider
@@ -29,39 +34,51 @@ class XTTS2:
         self.current_device = "cpu"
         self.tts_model_loaded = False
         self.xtts2_model_installed = False
-        
+
         # Only download XTTS2 model once someone enables it, alternative would be downloading on first run, which the code also will do if necessary
         if settings.enable:
             checkpoint_dir, config_path = self.download_xtts2_main_model()
-        #checkpoint_dir, config_path = self.download_xtts2_main_model()
+        # checkpoint_dir, config_path = self.download_xtts2_main_model()
 
     async def play_audio(
         self,
         text: str,
-        config: XTTS2TtsConfig, # needs to be created
+        config: XTTS2TtsConfig,  # needs to be created
         sound_config: SoundConfig,
         audio_player: AudioPlayer,
         wingman_name: str,
     ):
         streaming = False
-        #if not config.streaming_mode:
+        # if not config.streaming_mode:
         if not streaming:
             output_file = await self.__generate_speech(
-                text=text, voice=config.voice, temperature=config.temperature, speed=config.speed, language=config.language, device=config.device   # need these variables in config
+                text=text,
+                voice=config.voice,
+                temperature=config.temperature,
+                speed=config.speed,
+                language=config.language,
+                device=config.device,  # need these variables in config
             )
             audio, sample_rate = audio_player.get_audio_from_file(output_file)
-            
+
             await audio_player.play_with_effects(
                 input_data=(audio, sample_rate),
                 config=sound_config,
                 wingman_name=wingman_name,
             )
-        #elif config.streaming_mode:
+        # elif config.streaming_mode:
         elif streaming:
             await self.__generate_streaming_speech(
-                sound_config=sound_config, audio_player=audio_player, wingman_name=wingman_name, text=text, voice=config.voice, temperature=config.temperature, speed=config.speed, language=config.language, device=config.device,   
+                sound_config=sound_config,
+                audio_player=audio_player,
+                wingman_name=wingman_name,
+                text=text,
+                voice=config.voice,
+                temperature=config.temperature,
+                speed=config.speed,
+                language=config.language,
+                device=config.device,
             )
-            
 
     # Generate speech depending on whether using XTTS2 built in voices, cloning from wav files, or generating from shared latents (e.g. the output of wav file cloning)
     async def __generate_speech(
@@ -71,26 +88,28 @@ class XTTS2:
         temperature: float = 0.75,
         speed: float = 1.20,
         language: str = "en",
-        device: str = "cpu"
+        device: str = "cpu",
     ):
         if not text:
             return
-        
+
         # Allow users to just use "gpu" for less advanced users, and translate this to cuda, while allowing more advanced users to select gpu number by using cuda:0, cuda:1, etc.
         if device == "gpu":
             device = "cuda"
-        
+
         # Only load model once there's a request for generation; this adds a slight delay but ensures XTTS2 uses no resources unless the user really wants to use it this session
         if not self.tts or not self.tts_model_loaded:
             await self.load_xtts2(device)
-        
+
         await self.handle_vram_change(device)
-        
+
         completed_file_path = ""
-        
+
         file_path = path.join(get_writable_dir(RECORDING_PATH), OUTPUT_FILE)
-        
-        if not voice.startswith(CLONING_WAVS_PATH) and not voice.startswith(LATENTS_PATH):
+
+        if not voice.startswith(CLONING_WAVS_PATH) and not voice.startswith(
+            LATENTS_PATH
+        ):
             completed_file_path = self.tts.tts_to_file(
                 text=text,
                 speaker=voice,
@@ -101,6 +120,7 @@ class XTTS2:
             )
         elif voice.startswith(CLONING_WAVS_PATH):
             full_path = Path(get_writable_dir(voice))
+            voices_list = None
             if full_path.is_dir():
                 # It's a directory: collect all .wav files
                 voices_list = [
@@ -122,7 +142,9 @@ class XTTS2:
         elif voice.startswith(LATENTS_PATH):
             voice_with_extension = voice + ".json"
             voice_file_path = get_writable_dir(voice_with_extension)
-            gpt_cond_latent, speaker_embedding = self.load_latents_from_json(device, voice_file_path) # voice = "C:/OtherPrograms/Github/xtts2_test/mantella_latents/en/emperor.json"
+            gpt_cond_latent, speaker_embedding = self.load_latents_from_json(
+                device, voice_file_path
+            )  # voice = "C:/OtherPrograms/Github/xtts2_test/mantella_latents/en/emperor.json"
             out = self.tts.synthesizer.tts_model.inference(
                 text,
                 language,
@@ -136,10 +158,10 @@ class XTTS2:
             completed_file_path = file_path
 
         await self.handle_vram_change("cpu")
-        
+
         return completed_file_path
-    
-    def _process_model_stream(self, stream_iterator): #,sample_rate, sample_width):
+
+    def _process_model_stream(self, stream_iterator):  # ,sample_rate, sample_width):
         for chunk in stream_iterator:
             if chunk is None:
                 continue
@@ -156,7 +178,7 @@ class XTTS2:
             chunk = np.clip(chunk, -1.0, 1.0)
             int16_chunk = (chunk * 32767).astype(np.int16)
             yield int16_chunk.tobytes()
-            
+
     async def __generate_streaming_speech(
         self,
         sound_config: SoundConfig,
@@ -171,23 +193,26 @@ class XTTS2:
     ):
         if not text:
             return
-        
+
         # Allow users to just use "gpu" for less advanced users, and translate this to cuda, while allowing more advanced users to select gpu number by using cuda:0, cuda:1, etc.
         if device == "gpu":
             device = "cuda"
-        
+
         # Only load model once there's a request for generation; this adds a slight delay but ensures XTTS2 uses no resources unless the user really wants to use it this session
         if not self.tts or not self.tts_model_loaded:
             await self.load_xtts2(device)
-        
+
         await self.handle_vram_change(device)
-        
+
         if voice.startswith(LATENTS_PATH):
             voice_with_extension = voice + ".json"
             voice_file_path = get_writable_dir(voice_with_extension)
-            gpt_cond_latent, speaker_embedding = self.load_latents_from_json(device, voice_file_path)
+            gpt_cond_latent, speaker_embedding = self.load_latents_from_json(
+                device, voice_file_path
+            )
         elif voice.startswith(CLONING_WAVS_PATH):
             full_path = Path(get_writable_dir(voice))
+            voices_list = None
             if full_path.is_dir():
                 voices_list = [
                     str(wav_path)
@@ -196,10 +221,18 @@ class XTTS2:
                 ]
             elif full_path.is_file() and full_path.suffix == ".wav":
                 voices_list = [str(full_path)]
-            gpt_cond_latent, speaker_embedding = self.tts.synthesizer.tts_model.get_conditioning_latents(audio_path=voices_list)
+            gpt_cond_latent, speaker_embedding = (
+                self.tts.synthesizer.tts_model.get_conditioning_latents(
+                    audio_path=voices_list
+                )
+            )
         else:
-            gpt_cond_latent, speaker_embedding = self.self.tts.synthesizer.tts_model.get_conditioning_latents(speaker=voice, language=language) # to do, built in voices -> latents, see https://gist.github.com/ichabodcole/1c0d19ef4c33b7b5705b0860c7c27f7b
-        
+            gpt_cond_latent, speaker_embedding = (
+                self.tts.synthesizer.tts_model.get_conditioning_latents(
+                    speaker=voice, language=language
+                )
+            )  # to do, built in voices -> latents, see https://gist.github.com/ichabodcole/1c0d19ef4c33b7b5705b0860c7c27f7b
+
         output_iterator = self.tts.synthesizer.tts_model.inference_stream(
             text,
             language,
@@ -207,7 +240,7 @@ class XTTS2:
             speaker_embedding=speaker_embedding,
             temperature=temperature,
             speed=speed,
-            stream_chunk_size=20
+            stream_chunk_size=20,
             ##**self.tts_settings, # Expands the object with the settings and applies them for generation
         )
 
@@ -227,7 +260,7 @@ class XTTS2:
                 else:
                     incomplete_buffer = b""
 
-                audio_buffer[:len(chunk)] = chunk
+                audio_buffer[: len(chunk)] = chunk
                 return len(chunk)
             except StopIteration:
                 if incomplete_buffer:
@@ -245,30 +278,33 @@ class XTTS2:
             channels=1,
             dtype="int16",
         )
-        
+
         await self.handle_vram_change("cpu")
-    
+
     # Move model in and out of VRAM between generations to reduce performance impact - TBD if this should be async
     async def handle_vram_change(self, desired_device):
         if not self.tts:
             return
         self.current_device = str(self.tts.synthesizer.tts_model.device)
         if torch.cuda.is_available():
-            print(self.tts.synthesizer.tts_model.device)
+            printr.print(
+                f"XTTS2 synth device: {self.tts.synthesizer.tts_model.device}",
+                server_only=True,
+            )
             if "cuda" in desired_device:
                 if "cuda" not in self.current_device:
-                    print("Moving XTTS2 model to GPU")
+                    printr.print("XTTS2: Moving XTTS2 model to GPU", server_only=True)
                     self.tts.synthesizer.tts_model.to(desired_device)
                     gc.collect()
                     self.current_device = desired_device
             if "cpu" in desired_device:
                 if "cpu" not in self.current_device:
-                    print("Moving model to CPU")
+                    printr.print("XTTS2: Moving model to CPU", server_only=True)
                     self.tts.synthesizer.tts_model.to(desired_device)
                     torch.cuda.empty_cache()
                     gc.collect()
                     self.current_device = desired_device
-    
+
     # Generic helper functions
     def create_directory_if_not_exists(self, directory):
         directory = Path(directory)
@@ -276,27 +312,29 @@ class XTTS2:
             directory.mkdir(parents=True, exist_ok=True)
 
     def download_file(self, url, destination):
-        response = requests.get(url, stream=True)
-        total_size_in_bytes = int(response.headers.get('content-length', 0))
+        response = requests.get(url, stream=True, timeout=10)
+        total_size_in_bytes = int(response.headers.get("content-length", 0))
         block_size = 1024  # 1 Kibibyte
-        print(f"Downloading file at {url}, please wait...")
+        printr.print(
+            f"XTTS2: Downloading file at {url}, please wait...", server_only=True
+        )
 
-        with open(destination, 'wb') as file:
+        with open(destination, "wb") as file:
             for data in response.iter_content(block_size):
                 file.write(data)
-        print(f"File downloaded to {destination}.")
+        printr.print(f"XTTS2: File downloaded to {destination}.", server_only=True)
 
     def download_model(self, this_dir, model_version):
         # Define paths
         base_path = this_dir
-        model_path = base_path / f'{model_version}'
+        model_path = base_path / f"{model_version}"
 
         # Define files and their corresponding URLs
         files_to_download = {
-             "config.json": f"https://huggingface.co/coqui/XTTS-v2/raw/{model_version}/config.json",
-             "model.pth": f"https://huggingface.co/coqui/XTTS-v2/resolve/{model_version}/model.pth?download=true",
-             "vocab.json": f"https://huggingface.co/coqui/XTTS-v2/raw/{model_version}/vocab.json",
-             "speakers_xtts.pth": "https://huggingface.co/coqui/XTTS-v2/resolve/main/speakers_xtts.pth?download=true"
+            "config.json": f"https://huggingface.co/coqui/XTTS-v2/raw/{model_version}/config.json",
+            "model.pth": f"https://huggingface.co/coqui/XTTS-v2/resolve/{model_version}/model.pth?download=true",
+            "vocab.json": f"https://huggingface.co/coqui/XTTS-v2/raw/{model_version}/vocab.json",
+            "speakers_xtts.pth": "https://huggingface.co/coqui/XTTS-v2/resolve/main/speakers_xtts.pth?download=true",
         }
 
         # Check and create directories
@@ -305,26 +343,26 @@ class XTTS2:
 
         # Download files if they don't exist
         for filename, url in files_to_download.items():
-             destination = model_path / filename
-             if not destination.exists():
-                 print(f"[XTTS2] Downloading {filename}...")
-                 self.download_file(url, destination)
+            destination = model_path / filename
+            if not destination.exists():
+                printr.print(f"XTTS2: Downloading {filename}...", server_only=True)
+                self.download_file(url, destination)
         return True
-    
+
     def load_latents_from_json(self, device, file_path):
-        with open(file_path, 'r') as json_file:
+        with open(file_path, "r", encoding="UTF-8") as json_file:
             data = json.load(json_file)
         # Use the class's device setting for tensor allocation
-        gpt_cond_latent = torch.tensor(data['gpt_cond_latent'], device=device)
-        speaker_embedding = torch.tensor(data['speaker_embedding'], device=device)
+        gpt_cond_latent = torch.tensor(data["gpt_cond_latent"], device=device)
+        speaker_embedding = torch.tensor(data["speaker_embedding"], device=device)
         return gpt_cond_latent, speaker_embedding
-    
+
     # Use to download the main xtts2 model files and store them in a designated directory
-    def download_xtts2_main_model(self):  
+    def download_xtts2_main_model(self):
         this_dir = Path(get_writable_dir(MODEL_DIR))
         model_version = "main"
-        config_path = this_dir / f'{model_version}' / 'config.json'
-        checkpoint_dir = this_dir / f'{model_version}'
+        config_path = this_dir / f"{model_version}" / "config.json"
+        checkpoint_dir = this_dir / f"{model_version}"
         # Return if we know already installed
         if self.xtts2_model_installed:
             return checkpoint_dir, config_path
@@ -334,7 +372,7 @@ class XTTS2:
         self.xtts2_model_installed = True
         # Return the paths we need for the checkpoint dir and config path
         return checkpoint_dir, config_path
-    
+
     # Load the XTTS2 model into memory
     async def load_xtts2(self, device):
         # If we already know tts model is loaded, skip
@@ -345,6 +383,5 @@ class XTTS2:
         checkpoint_dir, config_path = self.download_xtts2_main_model()
 
         # Initialize TTS
-        self.tts = TTS(model_path=checkpoint_dir,config_path=config_path).to(device)
+        self.tts = TTS(model_path=checkpoint_dir, config_path=config_path).to(device)
         self.tts_model_loaded = True
-    
