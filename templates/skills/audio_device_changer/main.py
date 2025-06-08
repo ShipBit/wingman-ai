507e8b002abbfdd57190847a48a4fdf056a7e608
Simon Hopstätter <simon.hopstaetter@shipbit.de>
2025-06-08 15:42:20 +0200

    update skill template

--- templates/skills/audio_device_changer/main.py
+++ templates/skills/audio_device_changer/main.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 import aiohttp
 from api.interface import (
     AudioDeviceSettings,
@@ -24,15 +24,15 @@ class AudioDeviceChanger(Skill):
     ) -> None:
         super().__init__(config=config, settings=settings, wingman=wingman)
         self.original_audio_device = settings.audio.output
-        self.audio_device: Optional[int | AudioDeviceSettings] = None
+        self.current_audio_device = settings.audio.output
         self.backend_port = 49111
+        self.wingman.audio_player.playback_events.subscribe(
+            "finished", self.playback_finished
+        )
 
     async def validate(self) -> list[WingmanInitializationError]:
         errors = await super().validate()
 
-        self.audio_device = self.retrieve_custom_property_value(
-            "audio_changer_device", errors
-        )
         backend_port = self.retrieve_custom_property_value("backend_port", errors)
         if backend_port is not None:
             self.backend_port = backend_port
@@ -42,11 +42,11 @@ class AudioDeviceChanger(Skill):
     async def _change_audio_device(self, device_id: int | AudioDeviceSettings) -> bool:
         """Change the audio output device via HTTP request."""
         try:
-            payload = {"audio": {"output": device_id}}
-
             async with aiohttp.ClientSession() as session:
                 async with session.post(
-                    f"http://127.0.0.1:{self.backend_port}/settings", json=payload
+                    f"http://127.0.0.1:{self.backend_port}/settings/audio-devices?output_device={device_id}"
+                    if device_id
+                    else f"http://127.0.0.1:{self.backend_port}/settings/audio-devices"
                 ) as response:
                     if response.status == 200:
                         self.printr.print(
@@ -69,16 +69,44 @@ class AudioDeviceChanger(Skill):
             return False
 
     async def on_play_to_user(self, text: str, sound_config: SoundConfig):
-        if self.audio_device is not None:
-            # Change to the target audio device
-            await self._change_audio_device(self.audio_device)
+        errors: list[WingmanInitializationError] = []
+        audio_device = self.retrieve_custom_property_value(
+            "audio_changer_device", errors
+        )
+        if len(errors) > 0:
+            await self.printr.print_async(
+                f"Audio Device Changer: Error retrieving audio device settings: {errors[0].message}",
+                LogType.ERROR,
+            )
+        elif audio_device is not None and audio_device != self.original_audio_device:
+            self.current_audio_device = audio_device
+            await self._change_audio_device(audio_device)
+        return text
+
+    async def playback_finished(self, _):
+        await self.reset_audio_device()
 
     async def unload(self) -> None:
-        # Reset the audio device to the original one
-        if self.original_audio_device is not None:
-            await self._change_audio_device(self.original_audio_device)
-            self.printr.print(
-                text="Audio Device Changer Skill unloaded. Reset audio device to original.",
-                color=LogType.INFO,
-                server_only=True,
-            )
+        await self.reset_audio_device()
+
+        self.wingman.audio_player.playback_events.unsubscribe(
+            "finished", self.playback_finished
+        )
+
+        self.printr.print(
+            text="Audio Device Changer Skill unloaded.",
+            color=LogType.INFO,
+            server_only=True,
+        )
+
+    async def reset_audio_device(self) -> None:
+        """Resets the audio device to the original one"""
+
+        if self.current_audio_device == self.original_audio_device:
+            return
+        await self._change_audio_device(self.original_audio_device)
+        self.printr.print(
+            text="Audio Device Changer: Reset audio device to original.",
+            color=LogType.INFO,
+            server_only=True,
+        )
