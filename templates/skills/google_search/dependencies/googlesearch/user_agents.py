e2ce3895d7a8c4ed03f167a9d6fa41d55bda7c43
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2025-09-21 10:16:11 -0400

    add google search skill

---
+++ templates/skills/google_search/dependencies/googlesearch/user_agents.py
@@ -0,0 +1,25 @@
+import random
+
+def get_useragent():
+    """
+    Generates a random user agent string mimicking the format of various software versions.
+
+    The user agent string is composed of:
+    - Lynx version: Lynx/x.y.z where x is 2-3, y is 8-9, and z is 0-2
+    - libwww version: libwww-FM/x.y where x is 2-3 and y is 13-15
+    - SSL-MM version: SSL-MM/x.y where x is 1-2 and y is 3-5
+    - OpenSSL version: OpenSSL/x.y.z where x is 1-3, y is 0-4, and z is 0-9
+
+    Returns:
+        str: A randomly generated user agent string.
+    """
+    #lynx_version = f"Lynx/{random.randint(2, 3)}.{random.randint(8, 9)}.{random.randint(0, 2)}"
+    #libwww_version = f"libwww-FM/{random.randint(2, 3)}.{random.randint(13, 15)}"
+    #ssl_mm_version = f"SSL-MM/{random.randint(1, 2)}.{random.randint(3, 5)}"
+    #openssl_version = f"OpenSSL/{random.randint(1, 3)}.{random.randint(0, 4)}.{random.randint(0, 9)}"
+    #return f"{lynx_version} {libwww_version} {ssl_mm_version} {openssl_version}"
+    user_agent_choices = [
+    "Mozilla/4.0 (PSP (PlayStation Portable); 2.00)", # definitely working
+    "AdsBot-Google (+http://www.google.com/adsbot.html)", # definitely working
+    ]
+    return random.choice(user_agent_choices)
