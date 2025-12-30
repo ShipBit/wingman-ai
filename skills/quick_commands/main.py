7d40d1c31d3f487c472726dbe9343516e7cbec6a
Jan Winkler <jan@jmwinkler.de>
2025-12-30 03:45:45 +0100

    quickcommands - fix os import

--- skills/quick_commands/main.py
+++ skills/quick_commands/main.py
@@ -1,4 +1,4 @@
-from os import path
+import os
 import json
 import datetime
 from typing import TYPE_CHECKING
@@ -21,8 +21,8 @@ class QuickCommands(Skill):
         super().__init__(config=config, settings=settings, wingman=wingman)
 
         # get file paths
-        self.data_path = path.join(self.get_generated_files_dir(), "data")
-        self.file_ipl = path.join(self.data_path, "instant_phrase_learning.json")
+        self.data_path = os.path.join(self.get_generated_files_dir(), "data")
+        self.file_ipl = os.path.join(self.data_path, "instant_phrase_learning.json")
 
         # learning data
         self.learning_blacklist = []
@@ -303,8 +303,8 @@ class QuickCommands(Skill):
             "learning_learned": self.learning_learned,
         }
 
-        dirpath = path.dirname(self.file_ipl)
-        if dirpath and not path.exists(dirpath):
-            __import__("os").makedirs(dirpath, exist_ok=True)
+        dirpath = os.path.dirname(self.file_ipl)
+        if dirpath and not os.path.exists(dirpath):
+            os.makedirs(dirpath, exist_ok=True)
         with open(self.file_ipl, "w", encoding="utf-8") as file:
             json.dump(learning_data, file, indent=4)
