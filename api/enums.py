79d11bb9cff3094c5d8e440ff7b4be6bd8caafd8
teddybear082 <87204721+teddybear082@users.noreply.github.com>
2026-01-30 21:38:34 -0500

    add pocket_trs to enums

--- api/enums.py
+++ api/enums.py
@@ -134,6 +134,7 @@ class TtsProvider(Enum):
     OPENAI_COMPATIBLE = "openai_compatible"
     HUME = "hume"
     INWORLD = "inworld"
+    POCKET_TTS = "pocket_tts"
 
 
 class SttProvider(Enum):
@@ -195,6 +196,7 @@ class WingmanProTtsProvider(Enum):
     AZURE = "azure"
     OPENAI = "openai"
     INWORLD = "inworld"
+    POCKET_TTS = "pocket_tts"
 
 
 class McpTransportType(Enum):
